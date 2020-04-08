0101eaf830d07b619bf0b69d85a4d47a4206682f
emad.mohamed.ivas <emad@ivas.com.eg>
2020-04-08 19:53:08 +0200

    emad,edit Du unsub

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -128,9 +128,9 @@
               @if(session()->has('check_status_id') && session()->has('status') && session()->get('status') == 'active')
               <li class="nav-item ">
                 @if(request()->get('OpID') == omantel)
-                <a class="nav-link text-capitalize" href="{{route('front.unsub',['requestId' => session()->get('check_status_id') , 'userToken' => session()->get('userToken')])}}"><i class="icon_before fas fa-key-alt fa-lg"></i> @lang('front.unsub')</a>
+                <a class="nav-link text-capitalize" href="{{route('front.unsub')}}"><i class="icon_before fas fa-key-alt fa-lg"></i> @lang('front.unsub')</a>
                 @elseif(request()->get('OpID') == du)
-                <a class="nav-link text-capitalize" href="{{route('front.du_unsub',['requestId' => session()->get('check_status_id') , 'userToken' => session()->get('userToken')])}}"><i class="icon_before fas fa-key-alt fa-lg"></i> @lang('front.unsub')</a>
+                <a class="nav-link text-capitalize" href="{{route('front.du_unsub')}}"><i class="icon_before fas fa-key-alt fa-lg"></i> @lang('front.unsub')</a>
                 @endif
               </li>
               <li class="nav-item ">
