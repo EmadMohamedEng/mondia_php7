a1aa35e7c367351e712c80af8cdff4d7fd5c109c
mohamed mahmoud <m.mahmoud@iVAS.local>
2020-04-09 11:29:49 +0200

    make innodb

---
+++ database/migrations/new/2020_02_16_093648_myisam_to_innodb.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MyisamToInnodb extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $res = \DB::select("SELECT  CONCAT('ALTER TABLE `', table_name, '` ENGINE=InnoDB;') AS sql_statements FROM  information_schema.tables AS tb WHERE  table_schema = "."'".env('DB_DATABASE')."'"." AND  `ENGINE` = 'MyISAM' AND  `TABLE_TYPE` = 'BASE TABLE' ORDER BY table_name DESC");
+        foreach ($res as $re) {
+          \DB::statement($re->sql_statements);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('innodb', function (Blueprint $table) {
+            //
+        });
+    }
+}
