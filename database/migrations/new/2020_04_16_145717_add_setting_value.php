ef5e2b4a204e95968ec4298ca63c299d86c15c3d
emad.mohamed.ivas <emad@ivas.com.eg>
2020-04-16 16:23:58 +0200

    emad,merge mohamed3

--- database/migrations/new/2020_04_16_145717_add_setting_value.php
+++ database/migrations/new/2020_04_16_145717_add_setting_value.php
@@ -14,8 +14,8 @@ class AddSettingValue extends Migration
     public function up()
     {
         \DB::statement("INSERT INTO `settings` (`id`, `key`, `value`, `created_at`, `updated_at`, `type_id`, `order`) VALUES
-        (80, 'service_delete_flag', '0', '2020-04-16 12:54:33', '2020-04-16 12:55:24', 7, 0),
-        (81, 'provider_delete_flag', '1', '2020-04-16 12:54:53', '2020-04-16 12:54:53', 7, 0);");
+        (81, 'service_delete_flag', '0', '2020-04-16 12:54:33', '2020-04-16 12:55:24', 7, 0),
+        (82, 'provider_delete_flag', '1', '2020-04-16 12:54:53', '2020-04-16 12:54:53', 7, 0);");
     }
 
     /**
