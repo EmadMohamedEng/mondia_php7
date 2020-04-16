3fe3be8d550c2eed7ea77cc42ee716dabae2dbcf
baher Talal <baher@ivas.com.eg>
2020-04-16 10:18:30 +0200

    remove animate & fix height for slider

--- resources/views/front/list_content.blade.php
+++ resources/views/front/list_content.blade.php
@@ -33,7 +33,7 @@
         @foreach ($contents as $item)
         <div class="col-md-4 col-lg-4 col-xl-2 col-4 p-0">
             <div class="item">
-              <div class="card ovf-hidden rotate-in-center">
+              <div class="card ovf-hidden">
                 <a class="owl_content_img view overlay link_href" href="{{route('front.inner',['id' => $item->content_id])}}">
                   @php
                       if($item->type == 1){
