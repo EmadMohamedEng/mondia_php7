fc7ce1392ab0ad44441cf25f7a191b6f8adc7c15
yousefashrafi <yousef.ashraf@ivas.com.eg>
2020-04-16 15:20:47 +0200

    svg

--- resources/views/providers/input.blade.php
+++ resources/views/providers/input.blade.php
@@ -24,10 +24,10 @@
   </div>
 </div>
 
-<div class="form-group" id="cktextarea">
+<div class="form-group">
     <label class="col-sm-3 col-lg-2 control-label">Value *</label>
     <div class="col-sm-9 col-lg-10 controls">
-        <textarea class="form-control col-md-12 ckeditor" name="image"
+        <textarea class="form-control col-md-12" name="image"
         rows="6">
             @isset($provider->image)
                 {{$provider->image}}
