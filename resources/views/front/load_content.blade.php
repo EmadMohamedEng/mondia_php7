3f59b853b663204d69ad2d2181137014b9b062fb
emad.mohamed.ivas <emad@ivas.com.eg>
2020-04-18 18:36:41 +0200

    emad.edit

--- resources/views/front/load_content.blade.php
+++ resources/views/front/load_content.blade.php
@@ -19,11 +19,11 @@
             <div class="mask waves-effect waves-light rgba-white-slight"></div>
             </a>
         </a>
-        @if($item->service_id != 33)
+        {{--  @if($item->service_id != 33)  --}}
         <div class="card-body">
             <h4 class="card-title text-capitalize">{{$item->getTranslation('title',getCode())}}</h4>
         </div>
-        @endif
+        {{--  @endif  --}}
         </div>
     </div>
 </div>
