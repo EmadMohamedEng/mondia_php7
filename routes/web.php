3bbeb2f0122a24ea40bb03a5fc30929bffd2d9f0
emad.mohamed.ivas <emad@ivas.com.eg>
2020-04-21 17:13:27 +0200

    emad,remove post link for stc

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,7 @@ Route::get('du_pin_code', 'FrontController@du_pin_code')->name('front.du_pin_cod
 Route::get('du_delete_subscription/', 'FrontController@du_delete_subscription')->name('front.du_unsub');
 Route::get('du_logout','FrontController@du_logout')->name('front.du_logout');
 Route::get('du_set_session','FrontController@du_set_session');
-Route::get('post_stc_create','PostsController@post_stc');
+// Route::get('post_stc_create','PostsController@post_stc');
 // Route::get('du_landing', 'DuController@index');
 // Route::get('test_du_login', 'DuController@testOmanTelLogin');
 // Route::get('du_create_token', 'DuController@create_token');
