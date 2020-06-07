757880e15ff16acb02941312dacaa0884f2c815f
Baher Talal <baher@ivas.com.eg>
2020-06-07 16:38:17 +0200

    slider home page

--- front/js/script.js
+++ front/js/script.js
@@ -55,13 +55,13 @@ $('.owl_content_one').owlCarousel({
   center: false,
   responsive: {
     0: {
-      items: 2
+      items: 1
     },
     600: {
-      items: 2
+      items: 1
     },
     1000: {
-      items: 2
+      items: 1
     }
   }
 });
