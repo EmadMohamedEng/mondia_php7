f39fca6260f1b22031aa4f265d1b4f1f46f2f710
yousefashrafi <yousef.ashraf@ivas.com.eg>
2020-09-08 12:36:50 +0200

    mo sms posting wsdl table and model

--- app/MbcSendMt.php
+++ app/MbcSendMt.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class MbcSendMt extends Model
 {
-    //
+  protected $fillable = ['SmsID', 'MobileNo', 'Country', 'Operator', 'Shortcode', 'Msgtxt', 'Lang', 'ServiceID', 'Request', 'Response', 'ResponseCode'];
 }
--- database/migrations/new/mbc/2020_09_08_100806_create_mbc_send_mts_table.php
+++ database/migrations/new/mbc/2020_09_08_100806_create_mbc_send_mts_table.php
@@ -15,17 +15,17 @@ class CreateMbcSendMtsTable extends Migration
     {
         Schema::create('mbc_send_mts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('SmsID', 20);
+            $table->string('SmsID', 50);
             $table->string('MobileNo', 50);
-            $table->string('Country', 300);
-            $table->string('Operator', 20);
+            $table->string('Country', 50);
+            $table->string('Operator', 50);
             $table->string('Shortcode', 50);
             $table->string('Msgtxt', 300);
-            $table->string('Lang', 20);
+            $table->string('Lang', 10);
             $table->string('ServiceID', 50);
-            $table->string('Request', 300);
-            $table->string('Response', 300);
-            $table->string('ResponseCode', 300);
+            $table->string('Request', 500);
+            $table->string('Response', 500);
+            $table->string('ResponseCode', 50);
             $table->timestamps();
         });
     }
