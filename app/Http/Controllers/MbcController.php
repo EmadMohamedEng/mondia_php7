abdc3f1b3cd5a0c3ed734e11fa38dc15d0738300
yousefashrafi <yousef.ashraf@ivas.com.eg>
2020-09-08 09:26:27 +0200

    mo sms posting wsdl

--- app/Http/Controllers/MbcController.php
+++ app/Http/Controllers/MbcController.php
@@ -50,12 +50,7 @@ class MbcController extends Controller
 
     public function MO_SMS_Posting()
     {
-      /*$url = 'http://mbc.mobc.com:8030/SourceSmsOut/SmsIN.asmx?WSDL';
-
-      // $headers = [
-      //   'Content-Type: application/xml',
-      //   'charset: utf-8'
-      // ];
+      $URL = 'http://mbc.mobc.com:8030/SourceSmsOut/SmsIN.asmx?WSDL';
 
       $Xmldoc['SmsID'] = '3';
       $Xmldoc['MobileNo'] = '966535550107';
@@ -66,9 +61,9 @@ class MbcController extends Controller
       $Xmldoc['Lang'] = 'E';
       $Xmldoc['ServiceID'] = '2';
 
-      $parameters['UserName'] = 'webSourceOut';
-      $parameters['UserPass'] = '2015Source@SMS_mbc';
-      $parameters['Xmldoc'] = "<?xml version='1.0' encoding='utf-8'?>
+      $parameters = ['UserName' =>'webSourceOut',
+      'UserPass' => '2015Source@SMS_mbc',
+      'Xmldoc' => "<?xml version='1.0' encoding='utf-8'?>
 <Packet>
 <SMS>
 <SmsID>{$Xmldoc['SmsID']}</SmsID>
@@ -80,56 +75,51 @@ class MbcController extends Controller
 <Lang>{$Xmldoc['Lang']}</Lang>
 <ServiceID>{$Xmldoc['ServiceID']}</ServiceID>
 </SMS>
-</Packet>";*/
+</Packet>"];
+
+      $client = new SoapClient($URL);
+
+      $response = $client->GetSmsIN($parameters);
+
+      dd($response);
+
+  //     $xml_data = '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:tem="http://tempuri.org/">
+  //     <soap:Header/>
+  //     <soap:Body>
+  //        <tem:GetSmsIN>
+  //           <!--Optional:-->
+  //           <tem:UserName>webSourceOut</tem:UserName>
+  //           <!--Optional:-->
+  //           <tem:UserPass>2015Source@SMS_mbc</tem:UserPass>
+  //           <!--Optional:-->
+  //           <tem:xmldoc>
+  //           <Packet>
+  //      <SMS>
+  //          <SmsID>3</SmsID>
+  //          <MobileNo>962782777131</MobileNo>
+  //          <Country>jordan</Country>
+  //          <Operator>umniah</Operator>
+  //          <Shortcode>94099</Shortcode>
+  //          <Msgtxt>text 3</Msgtxt>
+  //          <lang>E</lang>
+  //          <ServiceID>2</ServiceID>
+  //      </SMS>
+  //  </Packet>
+  //           </tem:xmldoc>
+  //        </tem:GetSmsIN>
+  //     </soap:Body>
+  //  </soap:Envelope>';
+
+  //     $ch = curl_init($URL);
+  //     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
+  //     curl_setopt($ch, CURLOPT_POST, 1);
+  //     curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml_data");
+  //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  //     $output = curl_exec($ch);
+  //     curl_close($ch);
 
 
-      // $response = $this->SendRequestPost($url, json_encode($parameters), $headers);
-
-      // $client = new SoapClient($url);
-
-      // $response = $client->GetSmsIN($parameters);
-
-      $xml_data = '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:tem="http://tempuri.org/">
-      <soap:Header/>
-      <soap:Body>
-         <tem:GetSmsIN>
-            <!--Optional:-->
-            <tem:UserName>webSourceOut</tem:UserName>
-            <!--Optional:-->
-            <tem:UserPass>2015Source@SMS_mbc</tem:UserPass>
-            <!--Optional:-->
-            <tem:xmldoc>
-            <Packet>
-       <SMS>
-           <SmsID>3</SmsID>
-           <MobileNo>962782777131</MobileNo>
-           <Country>jordan</Country>
-           <Operator>umniah</Operator>
-           <Shortcode>94099</Shortcode>
-           <Msgtxt>text 3</Msgtxt>
-           <lang>E</lang>
-           <ServiceID>2</ServiceID>
-       </SMS>
-   </Packet>
-            </tem:xmldoc>
-         </tem:GetSmsIN>
-      </soap:Body>
-   </soap:Envelope>';
-
-      $URL = "http://mbc.mobc.com:8030/SourceSmsOut/SmsIN.asmx?WSDL";
-
-      $ch = curl_init($URL);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
-      curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml_data");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      $output = curl_exec($ch);
-      curl_close($ch);
-
-
-      print_r($output);
-
-      // dd($response);
+  //     print_r($output);
 
     }
 }
