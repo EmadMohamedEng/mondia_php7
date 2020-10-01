1e1aa62f32f3d5e486e4a04959de81149f6b8f4a
Baher Talal <baher@ivas.com.eg>
2020-10-01 16:18:44 +0200

    mbc done

--- resources/views/front/profile.blade.php
+++ resources/views/front/profile.blade.php
@@ -1,17 +1,7 @@
 @extends('front.master')
 @section('page_title') @lang('front.profile') @endsection
 @section('content')
-<style>
-  .color_table {
-    color: #efc049;
-    font-weight: bold;
-  }
 
-  .table-bordered td,
-  .table-bordered th {
-    border: unset;
-  }
-</style>
 
 <section class="profile">
   @if(request()->get('OpID') == mbc)
@@ -19,43 +9,43 @@
     @else
     <div class="col-md-12 col-lg-12 col-xl-8 col-12 p-0 no_padding close_nav">
       @endif
-      <div>
-        <h2 style="color: white;text-align: center; padding-top: 2%;color: #efc049;">@lang('front.profile')</h2>
+      <div class="profile_title m-auto text-center">
+        <h2>@lang('front.profile')</h2>
       </div>
-      @php
+
+      {{-- @php
       $style_align = session()->get('applocale') == 'en' ? 'color: white;text-align: left' : 'color: white;text-align: right';
-      @endphp
-      <div style="padding: 4%;">
-        <table class="table table-striped table-bordered">
-          <tbody style="{{$style_align}}">
+      @endphp --}}
+
+      <div class="profile_table">
+        <table class="table  table-bordered ">
+          <tbody>
             <tr>
-              <td class="label-view color_table">@lang('messages.users.phone')</td>
-              <td style="{{$style_align}}">{{session()->get('MSISDN')}}</td>
+              <td class="label-view font-weight-bold">@lang('messages.users.phone')</td>
+              <td class="label-view2">{{session()->get('MSISDN')}}</td>
             </tr>
 
             <tr>
-              <td class="label-view color_table"> @lang('messages.users.date')</td>
-              <td>{{date('Y-M-d')}}</td>
+              <td class="label-view font-weight-bold"> @lang('messages.users.date')</td>
+              <td class="label-view2">{{date('Y-M-d')}}</td>
             </tr>
 
             <tr>
-              <td class="label-view color_table">@lang('messages.service_name')</td>
-              <td>@lang('messages.Mbc_Muslim_guide')</td>
+              <td class="label-view font-weight-bold">@lang('messages.service_name')</td>
+              <td class="label-view2">@lang('messages.Mbc_Muslim_guide')</td>
             </tr>
 
             <tr>
-              <td class="label-view color_table">@lang('messages.service_type')</td>
-              <td>@lang('messages.repeat.weekly')</td>
+              <td class="label-view font-weight-bold">@lang('messages.service_type')</td>
+              <td class="label-view2">@lang('messages.repeat.weekly')</td>
             </tr>
 
             <tr>
-              <td class="label-view color_table">@lang('messages.description')</td>
-              <td>@lang('messages.discription_weekly')</td>
+              <td class="label-view font-weight-bold">@lang('messages.description')</td>
+              <td class="label-view2">@lang('messages.discription_weekly')</td>
             </tr>
-
           </tbody>
         </table>
       </div>
-
-      @stop
 </section>
+@stop
