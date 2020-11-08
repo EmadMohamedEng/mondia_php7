6b1a25b4535effb4af027d23540b147014f884b0
emad.mohamed.ivas <emad@ivas.com.eg>
2020-11-08 09:45:02 -0500

    omar push yousef for dev

--- app/Http/Controllers/MbcTwoController.php
+++ app/Http/Controllers/MbcTwoController.php
@@ -512,30 +512,14 @@ class MbcTwoController extends Controller
       }
       $purpose = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
       $support = array("country", "countrycode", "state", "region", "city", "location", "address");
-      $continents = array(
-          "AF" => "Africa",
-          "AN" => "Antarctica",
-          "AS" => "Asia",
-          "EU" => "Europe",
-          "OC" => "Australia (Oceania)",
-          "NA" => "North America",
-          "SA" => "South America"
-      );
       if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
-          //  $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
-
           $ipdat  = @json_decode($this->GetPageData("http://www.geoplugin.net/json.gp?ip=" . $ip));
 
           if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
               switch ($purpose) {
                   case "location":
                       $output = array(
-                          "city" => @$ipdat->geoplugin_city,
-                          "state" => @$ipdat->geoplugin_regionName,
                           "country" => @$ipdat->geoplugin_countryName,
-                          "country_code" => @$ipdat->geoplugin_countryCode,
-                          "continent" => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
-                          "continent_code" => @$ipdat->geoplugin_continentCode
                       );
                       break;
                   case "address":
@@ -546,20 +530,8 @@ class MbcTwoController extends Controller
                           $address[] = $ipdat->geoplugin_city;
                       $output = implode(", ", array_reverse($address));
                       break;
-                  case "city":
-                      $output = @$ipdat->geoplugin_city;
-                      break;
-                  case "state":
-                      $output = @$ipdat->geoplugin_regionName;
-                      break;
-                  case "region":
-                      $output = @$ipdat->geoplugin_regionName;
-                      break;
                   case "country":
                       $output = @$ipdat->geoplugin_countryName;
-                      break;
-                  case "countrycode":
-                      $output = @$ipdat->geoplugin_countryCode;
                       break;
               }
           }
