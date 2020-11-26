dcc5bbcd5205f5127d2b901e13a66b70ec308e21
omar tarek <omar.tarek@iVAS.local>
2020-11-26 16:47:25 +0200

    fix lang for orange

---
+++ database/migrations/new/mbc/2020_11_26_134125_fix_respons.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixRespons extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+      \DB::statement("ALTER TABLE `response_send_messages` CHANGE `response` `response` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;");
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
