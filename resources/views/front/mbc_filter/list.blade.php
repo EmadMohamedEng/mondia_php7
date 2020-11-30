5eafe2597fa88e1abe41dcdc928bec9d663dcd62
Baher Talal <57946827+baher-talal@users.noreply.github.com>
2020-11-30 16:52:34 +0200

    inner filter

--- resources/views/front/mbc_filter/list.blade.php
+++ resources/views/front/mbc_filter/list.blade.php
@@ -13,112 +13,114 @@ $menu = provider_menu();
     @endif
 
     <section class="list_filter mt-5">
-      <div class="row">
-        <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
-          <div class="card ovf-hidden">
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <img class="w-100" src="front/images/mbc_filter/01.png" alt="filter">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
+            <div class="card ovf-hidden">
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <img class="w-100" src="front/images/mbc_filter/01.png" alt="filter">
 
-              <a>
-                <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                <a>
+                  <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                </a>
               </a>
-            </a>
 
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <div class="card-body">
-                <h4 class="card-title text-capitalize">دعوتك</h4>
-              </div>
-            </a>
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <div class="card-body">
+                  <h4 class="card-title text-capitalize">دعوتك</h4>
+                </div>
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
-          <div class="card ovf-hidden">
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <img class="w-100" src="front/images/mbc_filter/02.png" alt="filter">
+          <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
+            <div class="card ovf-hidden">
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <img class="w-100" src="front/images/mbc_filter/02.png" alt="filter">
 
-              <a>
-                <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                <a>
+                  <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                </a>
               </a>
-            </a>
 
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <div class="card-body">
-                <h4 class="card-title text-capitalize">سجود الشكر</h4>
-              </div>
-            </a>
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <div class="card-body">
+                  <h4 class="card-title text-capitalize">سجود الشكر</h4>
+                </div>
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
-          <div class="card ovf-hidden">
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <img class="w-100" src="front/images/mbc_filter/03.png" alt="filter">
+          <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
+            <div class="card ovf-hidden">
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <img class="w-100" src="front/images/mbc_filter/03.png" alt="filter">
 
-              <a>
-                <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                <a>
+                  <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                </a>
               </a>
-            </a>
 
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <div class="card-body">
-                <h4 class="card-title text-capitalize">القراًن حياتي</h4>
-              </div>
-            </a>
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <div class="card-body">
+                  <h4 class="card-title text-capitalize">القراًن حياتي</h4>
+                </div>
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
-          <div class="card ovf-hidden">
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <img class="w-100" src="front/images/mbc_filter/04.png" alt="filter">
+          <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
+            <div class="card ovf-hidden">
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <img class="w-100" src="front/images/mbc_filter/04.png" alt="filter">
 
-              <a>
-                <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                <a>
+                  <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                </a>
               </a>
-            </a>
 
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <div class="card-body">
-                <h4 class="card-title text-capitalize">دعوتك</h4>
-              </div>
-            </a>
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <div class="card-body">
+                  <h4 class="card-title text-capitalize">دعوتك</h4>
+                </div>
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
-          <div class="card ovf-hidden">
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <img class="w-100" src="front/images/mbc_filter/05.png" alt="filter">
+          <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
+            <div class="card ovf-hidden">
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <img class="w-100" src="front/images/mbc_filter/05.png" alt="filter">
 
-              <a>
-                <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                <a>
+                  <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                </a>
               </a>
-            </a>
 
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <div class="card-body">
-                <h4 class="card-title text-capitalize">سجود الشكر</h4>
-              </div>
-            </a>
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <div class="card-body">
+                  <h4 class="card-title text-capitalize">سجود الشكر</h4>
+                </div>
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
-          <div class="card ovf-hidden">
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <img class="w-100" src="front/images/mbc_filter/06.png" alt="filter">
+          <div class="col-md-6 col-lg-6 col-xl-4 col-6 p-1">
+            <div class="card ovf-hidden">
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <img class="w-100" src="front/images/mbc_filter/06.png" alt="filter">
 
-              <a>
-                <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                <a>
+                  <div class="mask waves-effect waves-light rgba-white-slight"></div>
+                </a>
               </a>
-            </a>
 
-            <a href="{{url('mbc_filter_inner/?OpID=14')}}">
-              <div class="card-body">
-                <h4 class="card-title text-capitalize">القراًن حياتي</h4>
-              </div>
-            </a>
+              <a href="{{url('mbc_filter_inner/?OpID=14')}}">
+                <div class="card-body">
+                  <h4 class="card-title text-capitalize">القراًن حياتي</h4>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
