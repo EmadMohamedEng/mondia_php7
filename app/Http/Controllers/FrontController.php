06b213e11fcce8d137a5716e009e59da3008007f
omar tarek <omar.tarek@iVAS.local>
2020-12-01 16:35:14 +0200

    make migrate free

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -1622,9 +1622,9 @@ class FrontController extends Controller
   {
     if(get_setting('filters_flag')){
       $operator = Operator::find($request->OpID);
-  
+
       $filters = $operator->filterPosts->where('filter_posts.published_date', '<=', \Carbon\Carbon::now()->format('Y-m-d'));
-  
+
       return view('front.mbc_filter.list', compact('filters'));
     }
   }
@@ -1644,5 +1644,5 @@ class FrontController extends Controller
       return redirect('mbc_portal_landing');
     }
   }
-  
+
 }
