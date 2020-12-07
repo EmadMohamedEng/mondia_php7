d5f44a370724c063d5725ca0b7512b15bb05eb88
emad.mohamed.ivas <emad@ivas.com.eg>
2020-12-07 10:40:11 +0200

    emad,push

--- routes/web.php
+++ routes/web.php
@@ -157,8 +157,6 @@ Route::get('mbc_portal_logout', 'MbcController@logout');
 Route::get('orange_portal_login', 'OrangeController@login');
 Route::get('orange_portal_unsub', 'OrangeController@unsubscribe');
 
-Route::get('orange_logout', 'OrangeController@logout');
-
 Route::post('orange_portal_login', 'OrangeController@postLogin');
 /***************end orange ****************** */
 
