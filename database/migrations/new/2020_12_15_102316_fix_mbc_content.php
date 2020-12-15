77f132b0f31720490e92980a031f4a84f63a6bcd
omar tarek <omar.tarek@iVAS.local>
2020-12-15 14:27:01 +0200

    finished mbc content

--- database/migrations/new/2020_12_15_102316_fix_mbc_content.php
+++ database/migrations/new/2020_12_15_102316_fix_mbc_content.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class MakeNullForMbContent extends Migration
+class FixMbcContent extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,7 @@ class MakeNullForMbContent extends Migration
      */
     public function up()
     {
-        \DB::statement("ALTER TABLE `mbc_contents` CHANGE `operator` `operator` VARCHAR(90) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL;");
-        \DB::statement("ALTER TABLE `mbc_contents` CHANGE `subscription_day` `subscription_day` INT(10) NULL;");
+        \DB::statement("ALTER TABLE `mbc_contents` CHANGE `subscription_day` `subscription_day` INT(10) NULL DEFAULT NULL;");
     }
 
     /**
