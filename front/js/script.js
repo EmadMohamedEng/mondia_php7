59352b95990902e2a8e7d8eb6afde00847e9dd8e
Baher Talal <57946827+baher-talal@users.noreply.github.com>
2020-12-23 16:50:09 +0200

    mbc iphone 5

--- front/js/script.js
+++ front/js/script.js
@@ -72,17 +72,20 @@ $('.owl_one').owlCarousel({
   autoplayTimeout: 3000,
   animateOut: 'fadeOut',
   nav: false,
-  dots: true,
+  dots: false,
   center: false,
   responsive: {
     0: {
-      items: 1
+      items: 1,
+      dots: true
     },
     600: {
-      items: 1
+      items: 1,
+      dots: true
     },
     1000: {
-      items: 3
+      items: 3,
+      dots: true
     },
     1024.9: {
       items: 3
